ac4252838a24e5c24d19c9e118911cdab894e432
tamim-18 <tamim11903060@gmail.com>
2025-08-22 23:10:38 +0600

    (add): all stock related problems

---
+++ Assignments/Best Time to Buy and Sell Stock IV.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        n=len(prices)
+        memo={}
+        def dp(day: int, stock: int, transaction: int) -> int:
+            if day == n or transaction >= k:
+                return 0
+            if (day, stock, transaction) in memo:
+                return memo[(day, stock, transaction)]
+            if stock == 0:
+                ## buy
+                buy = -prices[day] + dp(day + 1, 1, transaction)
+                keep = 0 + dp(day + 1, 0, transaction)
+                profit = max(buy, keep)
+                memo[(day, stock, transaction)] = profit
+                return profit
+            else:
+                ## sell
+                sell = prices[day] + dp(day + 1, 0, transaction + 1)
+                keep = 0 + dp(day + 1, 1, transaction)
+                profit = max(sell, keep)
+                memo[(day, stock, transaction)] = profit
+                return profit
+
+        return dp(0, 0, 0)
---
+++ Others Stock Problem/Best Time to Buy and Sell Stock with Transaction Fee.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        n = len(prices)
+        memo = {}
+
+        def dp(day: int, stock: int) -> int:
+            if day == n:
+                return 0
+            if (day, stock) in memo:
+                return memo[(day, stock)]
+            if stock == 0:
+                ## buy
+                buy = -prices[day] + dp(day + 1, 1)
+                keep = 0 + dp(day + 1, 0)
+                profit = max(buy, keep)
+                memo[(day, stock)] = profit
+                return profit
+            else:
+                ## sell
+                sell = prices[day] - fee + dp(day + 1, 0)
+                keep = 0 + dp(day + 1, 1)
+                profit = max(sell, keep)
+                memo[(day, stock)] = profit
+                return profit
+
+        return dp(0, 0)
